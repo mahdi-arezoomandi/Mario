2f100845b5aaf36e20d2d66b068291cb5f30eedf
mahdi-arezoomandi <mahdiarezand@gmail.com>
2025-05-04 16:09:02 +0430

    Add Camera Controller Script

--- My project/Assets/Scripts/Camera Controler.cs
+++ My project/Assets/Scripts/Camera Controler.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraControler : MonoBehaviour
 {
+    public Transform target;
+    public Vector3 New;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class CameraControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+        New = new Vector3(target.position.x, transform.position.y,-10);
+        if (target.position.x>= transform.position.x)
+            transform.position = Vector3.MoveTowards(transform.position, New, 5f*Time.deltaTime);
     }
 }
